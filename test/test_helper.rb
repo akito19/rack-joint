3380e14fbed32fdc230b9b8bbfd1f7a243023e7d
Akito Kasai <kasai@akito19.com>
2018-12-30 15:25:23 +0900

    Shorten config file name for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,33 +3,33 @@ require "rack/joint"
 require "minitest/autorun"
 
 def single_path_config
-  Rack::Builder.parse_file('test/config/set_single_path_config.ru').first
+  Rack::Builder.parse_file('test/config/redirect_config.ru').first
 end
 
 def multiple_paths_config
-  Rack::Builder.parse_file('test/config/set_multiple_paths_config.ru').first
+  Rack::Builder.parse_file('test/config/multiple_paths.ru').first
 end
 
 def multiple_hosts_config
-  Rack::Builder.parse_file('test/config/set_multiple_hosts_config.ru').first
+  Rack::Builder.parse_file('test/config/multiple_hosts.ru').first
 end
 
 def only_host_config
-  Rack::Builder.parse_file('test/config/set_only_host_config.ru').first
+  Rack::Builder.parse_file('test/config/change_host_with_each_path.ru').first
 end
 
 def same_host_config
-  Rack::Builder.parse_file('test/config/set_same_host_config.ru').first
+  Rack::Builder.parse_file('test/config/redirect_different_path_with_same_host.ru').first
 end
 
 def same_url_config
-  Rack::Builder.parse_file('test/config/set_same_url_config.ru').first
+  Rack::Builder.parse_file('test/config/set_same_url.ru').first
 end
 
 def get_url_with_ssl_config
-  Rack::Builder.parse_file('test/config/set_get_https_config.ru').first
+  Rack::Builder.parse_file('test/config/redirect_with_ssl.ru').first
 end
 
 def redirect_all_paths
-  Rack::Builder.parse_file('test/config/set_redirect_all_paths_config.ru').first
+  Rack::Builder.parse_file('test/config/change_host_with_all_paths.ru').first
 end

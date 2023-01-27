bc550a301eb20cae0be6b0689d89bad0cb0dbbc8
Akito Kasai <kasai@akito19.com>
2023-01-27 23:29:38 +0900

    Fix NoMethodError on test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,33 +3,33 @@ require "rack/joint"
 require "minitest/autorun"
 
 def single_path_config
-  Rack::Builder.parse_file('test/config/redirect_config.ru').first
+  Rack::Builder.parse_file('test/config/redirect_config.ru')
 end
 
 def multiple_paths_config
-  Rack::Builder.parse_file('test/config/multiple_paths.ru').first
+  Rack::Builder.parse_file('test/config/multiple_paths.ru')
 end
 
 def multiple_hosts_config
-  Rack::Builder.parse_file('test/config/multiple_hosts.ru').first
+  Rack::Builder.parse_file('test/config/multiple_hosts.ru')
 end
 
 def only_host_config
-  Rack::Builder.parse_file('test/config/change_host_with_each_path.ru').first
+  Rack::Builder.parse_file('test/config/change_host_with_each_path.ru')
 end
 
 def same_host_config
-  Rack::Builder.parse_file('test/config/redirect_different_path_with_same_host.ru').first
+  Rack::Builder.parse_file('test/config/redirect_different_path_with_same_host.ru')
 end
 
 def same_url_config
-  Rack::Builder.parse_file('test/config/set_same_url.ru').first
+  Rack::Builder.parse_file('test/config/set_same_url.ru')
 end
 
 def get_url_with_ssl_config
-  Rack::Builder.parse_file('test/config/redirect_with_ssl.ru').first
+  Rack::Builder.parse_file('test/config/redirect_with_ssl.ru')
 end
 
 def redirect_all_paths
-  Rack::Builder.parse_file('test/config/change_host_with_all_paths.ru').first
+  Rack::Builder.parse_file('test/config/change_host_with_all_paths.ru')
 end
